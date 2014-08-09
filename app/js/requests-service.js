0484b87d205ec61774d11bfa2c79a2d479795bc6
Brian Smith <brismuth@gmail.com>
2014-08-09 09:48:47 -0600

    Service request updates are now working correctly.

--- app/js/requests-service.js
+++ app/js/requests-service.js
@@ -6,10 +6,10 @@ angular.module('open311Client.requests_service.service', [
 .factory('requests_service', ['$http', '$q', 
                      function ($http,   $q) {
 
-  var baseUrl = 'http://311.zappala.org/';
+  var baseUrl = 'http://311.zappala.org/requests';
   var api_key = '32jjrntg5kjnf429q8h9q328h429f';
 
-  var requests = $http.get(baseUrl + 'requests.xml').then(function (result) {
+  var requests = $http.get(baseUrl + '.xml').then(function (result) {
     var x2js = new X2JS();
     return x2js.xml_str2json( result.data ).service_requests.request;
   });
@@ -20,7 +20,7 @@ angular.module('open311Client.requests_service.service', [
   };
 
   factory.get = function (service_request_id) {
-    var request = $http.get(baseUrl + 'requests/' + service_request_id + '.xml').then(function (result) {
+    var request = $http.get(baseUrl + '/' + service_request_id + '.xml').then(function (result) {
       var x2js = new X2JS();
       return x2js.xml_str2json( result.data ).service_requests.request;
     });
@@ -35,17 +35,7 @@ angular.module('open311Client.requests_service.service', [
       address_string: request.address
     };
 
-    var url = baseUrl;
-
-    if (request.service_request_id) {
-      params.service_request_id = request.service_request_id;
-      // this is a previously existing service request. Update it instead (TODO: this may not be the correct way of handling updates)
-      url += 'servicerequestupdates.xml';
-    } else {
-      // this is a new service request
-      url += 'requests.xml';
-    }
-
+    if (request.service_request_id) params.service_request_id = request.service_request_id;
     if (request.email) params.email = request.email;
     if (request.media_url) params.media_url = request.media_url;
     if (request.description) params.description = request.description;
@@ -57,7 +47,7 @@ angular.module('open311Client.requests_service.service', [
 
     $http({
       method: 'POST',
-      url: url,
+      url: baseUrl + '.xml',
       data: $.param(params),
       headers: {'Content-Type': 'application/x-www-form-urlencoded'}
     }).success(function (data) {
